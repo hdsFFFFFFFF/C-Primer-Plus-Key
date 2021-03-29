0e8ea26b40a4995928e5f1f0a67cb726a933c1b7
hdsFFFFFFFF <hdsFFFFFF@163.com>
2021-03-29 17:44:47 +0800

    fix cpt1_1

--- chapter-1/cpt1_1.c
+++ chapter-1/cpt1_1.c
@@ -7,6 +7,7 @@ void inch_into_cm(float *value)
 
 int main()
 {
+start:
 	printf("Please input the inch value:\n");
 
 	float inch_val;
@@ -14,6 +15,16 @@ int main()
 
 	inch_into_cm(&inch_val);
 	printf("cm:%f\n", inch_val);
+	
+	//goto start;
+	
+	printf("enter 'y' to goon, enter to exit\n");
+	//int select;
+	char select;
+	
+	scanf("%c", &select);
+	if (select == 'y')
+		goto start;
 
 	return 0;
 }
