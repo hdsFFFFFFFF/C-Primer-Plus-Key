8fa4ba625c2f7c5183ce21dfd97c65e9cd5d1e8d
hdsFFFFFFFF <hdsFFFFFF@163.com>
2021-04-02 22:37:07 +0800

    optimizate cpt3_2

--- chapter-1/cpt1_1.c
+++ chapter-1/cpt1_1.c
@@ -34,8 +34,8 @@ start:
 	do {
 		printf("Please input the inch value:\n");
 
-		scanf("%f", &inch_val);
-		fflush(stdin);
+		scanf("%f", &inch_val);/* 按下Enter后，'\n'字符留在了stdin中。如果紧接着调用getchar(),getchar()
+					  发现stdin中有字符'\n'，就直接读取了。*/
 		
 		double ret = inch_into_cm(inch_val);
 		printf("%lf\n", ret);
@@ -43,10 +43,10 @@ start:
 		
 		printf("Please input 'y' to goon, other to exit.\n");
 	
-		getchar();
+		getchar();	/* 吸收换行符 */
 		if (getchar() != 'y') {
 			select = 'n';
-			fflush(stdin);
+			//fflush(stdin);
 		}
 	} while (select == 'y');
 
--- chapter-3/cpt3_2.c
+++ chapter-3/cpt3_2.c
@@ -2,21 +2,24 @@
 
 int main(void)
 {
-start:
-	printf("Please input an ASCII value\n");
-
 	unsigned value;
-	scanf("%d", &value);
-
-	//ascii_trans(value);
-	if (value < 128)
-		printf("%c\n", value);
-	else
-		printf("error:overflow.Please try again\n");
+	char select = 'y';
 	
-	printf("Again? Please input 'y' to goon, other to exit\n");
-	if ('y' == getchar())
-		goto start;
+	do {
+		printf("Please input an ASCII value\n");
+
+		scanf("%d", &value);
+
+		if (value < 128)
+			printf("%c\n", value);
+		else
+			printf("error:overflow.Please try again\n");
+	
+		printf("Again? Please input 'y' to goon, other to exit\n");
+		getchar();
+		if ('y' != getchar())
+			select = 'n';
+	} while (select == 'y');
 
 	return 0;
 }
