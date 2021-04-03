48d3a10948a532096ee3ff3532db8303de434bd9
hdsFFFFFFFF <hdsFFFFFF@163.com>
2021-04-03 22:10:38 +0800

    rm code

--- chapter-1/cpt1_1.c
+++ chapter-1/cpt1_1.c
@@ -7,27 +7,6 @@ double inch_into_cm(float value)
 
 int main()
 {
-	/*
-start:
-	printf("Please input the inch value:\n");
-
-	float inch_val;
-	scanf("%f", &inch_val);
-
-	inch_into_cm(&inch_val);
-	printf("cm:%f\n", inch_val);
-	
-	//goto start;
-	
-	printf("enter 'y' to goon, enter to exit\n");
-	//int select;
-	char select;
-	
-	scanf("%c", &select);
-	if (select == 'y')
-		goto start;
-	*/
-
 	char select = 'y';
 	float inch_val;
 	
